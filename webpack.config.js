07eaae10a4c394375ebbe666ee37fb3e004175f0
Justin Zelinsky <jpzelinsky@gmail.com>
2018-10-11 21:15:37 -0400

    clear webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,17 @@
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const path = require('path');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-
-const devMode = process.env.NODE_ENV !== 'production';
 
 module.exports = {
   entry: './src/index.jsx',
   module: {
     rules: [
-      {
-        test: /.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: [
-          'babel-loader',
-          'eslint-loader'
-        ]
-      },
-      {
-        test: /.scss$/,
-        use: [
-          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              importLoaders: 1,
-              localIdentName: '[path]___[name]__[local]___[hash:base64:5]'
-            }
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              data: '@import "src/styles/vars";',
-              sourceMap: devMode === true
-            }
-          }
-        ]
-      }
+      // JavaScript loader
+      // ,
+      // CSS/SASS loader
     ]
   },
   optimization: {
     minimizer: [
-      new OptimizeCSSAssetsPlugin({}),
-      new UglifyJsPlugin({
-        cache: true,
-        parallel: true,
-        sourceMap: false
-      })
+      // Minimizer plugins
     ]
   },
   output: {
@@ -57,12 +19,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new HtmlWebpackPlugin({template: './src/index.html'}),
-    new MiniCssExtractPlugin({
-      filename: devMode ? '[name].css' : '[name].[hash].css',
-      chunkFilename: devMode ? '[id].css' : '[id].[hash].css'
-    })
+    // General plugins
   ],
   resolve: {
     extensions: ['.jsx', '.js'],
